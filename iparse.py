0736cd1c55a7ac618a6ed41da39b0b0ecbae3d03
ekkerri <ekkerri.cse@gmail.com>
2018-05-01 11:19:07 -0400

    potential issues with encryption and zipfile library not supporting encryption methods. Extract files earlier with 7z and handle/warn files that are empty.

--- iparse.py
+++ iparse.py
@@ -63,98 +63,11 @@ class IPArse(ServiceBase):
         except zipfile.BadZipfile:
             # Return if files cannot be extracted
             return
+        # isipa returns False if Info.plist not found, or returns Info.plist path
         name_list, isipa = self.isipa(ipa_file)
 
         if not isipa:
             return
-
-        # Info.plist
-        with open(os.path.join(os.path.dirname(__file__), "keys.json"), 'r') as f:
-            keys_dict = json.load(f)
-
-        infoplist_common = keys_dict['glossary']
-        info_plist = ipa_file.read(isipa)
-
-        try:
-            plist_parsed = plistlib.readPlistFromString(info_plist)
-        except:
-            try:
-                plist_parsed = biplist.readPlistFromString(info_plist)
-            except Exception as e:
-                self.log.info("Not a plist:", e)
-                return
-
-        res = ResultSection(SCORE.NULL, "Info.plist")
-        known = []
-        unknown = []
-        main_exe = ""
-        for k, i in plist_parsed.iteritems():
-            k_noipad = k.replace("~ipad", "")
-            # Grab the main executable name
-            if k == "CFBundleExecutable":
-                try:
-                    main_exe = (i, "Name of bundle's main executable file: {}" .format(i))
-                except UnicodeEncodeError:
-                    i = i.encode('utf8', 'replace')
-                    main_exe = (i, "Name of bundle's main executable file: {}".format(i))
-            elif k_noipad in infoplist_common:
-                try:
-                    known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
-                except UnicodeEncodeError:
-                    i = i.encode('utf8', 'replace')
-                    known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
-            else:
-                try:
-                    unknown.append("{}:  {}".format(k, i))
-                except UnicodeEncodeError:
-                    i = i.encode('utf8', 'replace')
-                    unknown.append("{}:  {}".format(k, i))
-                continue
-            if infoplist_common[k_noipad][1]:
-                if isinstance(i, list):
-                    for val in i:
-                        res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], val, TAG_WEIGHT.LOW)
-                else:
-                    res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], i, TAG_WEIGHT.LOW)
-
-        if main_exe != "":
-            res.add_line(main_exe[1])
-        if len(known) > 0:
-            idenkey_sec = ResultSection(SCORE.NULL, "Identified Keys", parent=res)
-            for r in sorted(known):
-                idenkey_sec.add_line(r)
-
-        if len(unknown) > 0:
-            unkkey_sec = ResultSection(SCORE.NULL, "UNIDENTIFIED KEYS:", parent=res)
-            unkkey_sec.add_line("Go to https://developer.apple.com/library/content/documentation/General/Reference"
-                                "/InfoPlistKeyReference/Introduction/Introduction.html")
-            for r in sorted(unknown):
-                unkkey_sec.add_line(r)
-
-        result.add_section(res)
-
-        # PkgInfo file
-        pkg_types = {
-            'APPL': 'application',
-            'FMWK': 'frameworks',
-            'BNDL': 'loadable bundle'
-        }
-        pattern = re.compile(r'Payload/[^/]*.app/PkgInfo')
-        for fn in name_list:
-            m = pattern.match(fn)
-            if m is not None:
-                pkg_info = ipa_file.read(m.group())
-                if len(pkg_info) == 8:
-                    try:
-                        pkgtype = pkg_info[0:4]
-                        if pkgtype in pkg_types:
-                            pkgtype = pkg_types[pkgtype]
-                        creator_code = pkg_info[4:]
-                        res = ResultSection(SCORE.NULL, "PkgInfo Details")
-                        res.add_line("Package Type: {}; Application Signature: {}".format(pkgtype, creator_code))
-                        result.add_section(res)
-                    except Exception:
-                        continue
 
         # Extract Files of interest using 7zip (some files can be AES encrypted which standard zipfile library does not
         # support)
@@ -168,8 +81,114 @@ class IPArse(ServiceBase):
         if not extract_success:
             return
 
+        # Info.plist
+        main_exe = None
+        res = ResultSection(SCORE.NULL, "Info.plist")
+        info_plist_path = os.path.join(wrk_dir, isipa)
+        with open(info_plist_path, 'r') as f:
+            info_plist = f.read()
+
+        if info_plist == "":
+            res.add_line("Empty Info.plist file. Archive contents may be encrypted.")
+        else:
+
+            with open(os.path.join(os.path.dirname(__file__), "keys.json"), 'r') as f:
+                keys_dict = json.load(f)
+
+            infoplist_common = keys_dict['glossary']
+
+            try:
+                plist_parsed = plistlib.readPlistFromString(info_plist)
+            except:
+                try:
+                    plist_parsed = biplist.readPlistFromString(info_plist)
+                except Exception as e:
+                    self.log.info("Not a plist:", e)
+                    return
+
+            known = []
+            unknown = []
+            main_exe = ""
+            for k, i in plist_parsed.iteritems():
+                k_noipad = k.replace("~ipad", "")
+                # Grab the main executable name
+                if k == "CFBundleExecutable":
+                    try:
+                        main_exe = (i, "Name of bundle's main executable file: {}" .format(i))
+                    except UnicodeEncodeError:
+                        i = i.encode('utf8', 'replace')
+                        main_exe = (i, "Name of bundle's main executable file: {}".format(i))
+                elif k_noipad in infoplist_common:
+                    try:
+                        known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
+                    except UnicodeEncodeError:
+                        i = i.encode('utf8', 'replace')
+                        known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
+                else:
+                    try:
+                        unknown.append("{}:  {}".format(k, i))
+                    except UnicodeEncodeError:
+                        i = i.encode('utf8', 'replace')
+                        unknown.append("{}:  {}".format(k, i))
+                    continue
+                if infoplist_common[k_noipad][1]:
+                    if isinstance(i, list):
+                        for val in i:
+                            res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], val, TAG_WEIGHT.LOW)
+                    else:
+                        res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], i, TAG_WEIGHT.LOW)
+
+            if main_exe != "":
+                res.add_line(main_exe[1])
+            if len(known) > 0:
+                idenkey_sec = ResultSection(SCORE.NULL, "Identified Keys", parent=res)
+                for r in sorted(known):
+                    idenkey_sec.add_line(r)
+
+            if len(unknown) > 0:
+                unkkey_sec = ResultSection(SCORE.NULL, "UNIDENTIFIED KEYS:", parent=res)
+                unkkey_sec.add_line("Go to https://developer.apple.com/library/content/documentation/General/Reference"
+                                    "/InfoPlistKeyReference/Introduction/Introduction.html")
+                for r in sorted(unknown):
+                    unkkey_sec.add_line(r)
+
+        result.add_section(res)
+
+        # PkgInfo file
+        pkg_types = {
+            'APPL': 'application',
+            'FMWK': 'frameworks',
+            'BNDL': 'loadable bundle'
+        }
+        pattern = re.compile(r'Payload/[^/]*.app/PkgInfo')
+        for fn in name_list:
+            m = pattern.match(fn)
+            if m is not None:
+                res = ResultSection(SCORE.NULL, "PkgInfo Details")
+                pkg_info_path = os.path.join(wrk_dir, m.group())
+                with open(pkg_info_path, 'r') as f:
+                    pkg_info = f.read()
+                if pkg_info == "":
+                    res.add_line("Empty PkgInfo file. Archive contents may be encrypted.")
+                elif len(pkg_info) == 8:
+                    try:
+                        pkgtype = pkg_info[0:4]
+                        if pkgtype in pkg_types:
+                            pkgtype = pkg_types[pkgtype]
+                        creator_code = pkg_info[4:]
+                        res = ResultSection(SCORE.NULL, "PkgInfo Details")
+                        res.add_line("Package Type: {}; Application Signature: {}".format(pkgtype, creator_code))
+                    except Exception:
+                        continue
+                result.add_section(res)
+
+        if main_exe:
+            main_exe_reg = (r'.*{}$' .format(main_exe[0]), "Main executable file {}" .format(main_exe[0]))
+        else:
+            main_exe_reg = ('$', 'Place holder for missing main executable name.')
+
         fextract_regs = [
-            (r'.*{}$' .format(main_exe[0]), "Main executable file {}" .format(main_exe[0])),
+            main_exe_reg,
             (r'.*\.(?:crt|cer|der|key|p12|p7b|p7c|pem|pfx)$', "Certificate or key file"),
             (r'.*libswift[^\/]\.dylib$', "Swift code library files"),
             (r'META-INF\/.*ZipMetadata.plist$', "IPA archive content info"),
@@ -177,12 +196,19 @@ class IPArse(ServiceBase):
             (r'.*plist$', "Plist information file"),
         ]
 
+        empty_file_msg = "Empty file. Archive contents may be encrypted."
         int_files = {}
         for root, dirs, files in os.walk(os.path.join(wrk_dir, "Payload")):
                 for name in files:
                     matched = False
                     full_path = safe_str(os.path.join(root, name))
-                    if os.path.getsize(full_path) < 50000001:
+                    if os.path.getsize(full_path) == 0:
+                        if int_files.get(empty_file_msg, None):
+                            int_files[empty_file_msg].append(full_path)
+                        else:
+                            int_files[empty_file_msg] = []
+                            int_files[empty_file_msg].append(full_path)
+                    elif os.path.getsize(full_path) < 50000001:
                         for p, desc in fextract_regs:
                             pattern = re.compile(p)
                             m = pattern.match(full_path.decode("utf8"))
