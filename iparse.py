8ccf7e3ad0e78622855e3d2ee064f0ceb1558b10
user <ekkerri@cse>
2018-05-31 13:39:17 -0400

    added new results for all plist info based on request.

--- iparse.py
+++ iparse.py
@@ -15,13 +15,16 @@ class IPArse(ServiceBase):
 
     def __init__(self, cfg=None):
         super(IPArse, self).__init__(cfg)
+        self.patterns = None
+        self.result = None
+        self.known_keys = None
 
     def start(self):
         self.log.debug("iParse service started")
 
     # noinspection PyUnresolvedReferences,PyGlobalUndefined
     def import_service_deps(self):
-        global biplist, json, os, plistlib, re, subprocess, unicodedata, zipfile
+        global biplist, json, os, PatternMatch, plistlib, re, subprocess, unicodedata, zipfile
         import biplist
         import os
         import json
@@ -30,6 +33,10 @@ class IPArse(ServiceBase):
         import subprocess
         import unicodedata
         import zipfile
+        try:
+            from al_services.alsvc_frankenstrings.balbuzard.patterns import PatternMatch
+        except ImportError:
+            PatternMatch = None
 
     def isipa(self, zf):
         # Help from https://herkuang.info/en/2016/01/22/extract-app-info-in-ipa-files-using-python/
@@ -51,9 +58,90 @@ class IPArse(ServiceBase):
             raise Exception(stderr)
         return
 
+    def extract_iocs(self, val):
+        st_value = self.patterns.ioc_match(val, bogon_ip=True)
+        if len(st_value) > 0:
+            for ty, val in st_value.iteritems():
+                if val == "":
+                    asc_asc = unicodedata.normalize('NFKC', val).encode('ascii', 'ignore')
+                    self.result.add_tag(TAG_TYPE[ty], asc_asc, TAG_WEIGHT.LOW)
+                else:
+                    ulis = list(set(val))
+                    for v in ulis:
+                        self.result.add_tag(TAG_TYPE[ty], v, TAG_WEIGHT.LOW)
+        return
+
+    def gen_plist_extract(self, plistfile):
+        # Get PLIST dictionary
+        empty = None
+        plist_dict = None
+        with open(plistfile, 'r') as f:
+            info_plist = f.read()
+
+        if info_plist == "":
+            empty = True
+            return empty, plist_dict
+        else:
+            try:
+                plist_dict = plistlib.readPlistFromString(info_plist)
+            except:
+                try:
+                    plist_dict = biplist.readPlistFromString(info_plist)
+                except Exception as e:
+                    return empty, plist_dict
+
+        # Find IOCs in plist
+        if self.patterns:
+            plist_str = json.dumps(plist_dict)
+            self.extract_iocs(plist_str)
+
+        return empty, plist_dict
+
+    def parse_plist(self, pdict):
+
+        idenkey_sec = None
+        unkkey_sec = None
+
+        known = set()
+        unknown = set()
+
+        for k, i in pdict.iteritems():
+            k_noipad = k.replace("~ipad", "")
+            if k_noipad in self.known_keys:
+                try:
+                    known.add("{} ({}):  {}".format(k, self.known_keys[k_noipad][0], i))
+                except UnicodeEncodeError:
+                    i = i.encode('utf8', 'replace')
+                    known.add("{} ({}):  {}".format(k, self.known_keys[k_noipad][0], i))
+            else:
+                try:
+                    unknown.add("{}:  {}".format(k, i))
+                except UnicodeEncodeError:
+                    i = i.encode('utf8', 'replace')
+                    unknown.add("{}:  {}".format(k, i))
+                continue
+            if self.known_keys[k_noipad][1]:
+                if isinstance(i, list):
+                    for val in i:
+                        self.result.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], val, TAG_WEIGHT.LOW)
+                else:
+                    self.result.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], i, TAG_WEIGHT.LOW)
+
+        if len(known) > 0:
+            idenkey_sec = ResultSection(SCORE.NULL, "Identified Keys")
+            for r in sorted(known):
+                idenkey_sec.add_line(r)
+
+        if len(unknown) > 0:
+            unkkey_sec = ResultSection(SCORE.NULL, "UNIDENTIFIED KEYS:")
+            for r in sorted(unknown):
+                unkkey_sec.add_line(r)
+
+        return idenkey_sec, unkkey_sec
+
     def execute(self, request):
-        result = Result()
-        request.result = result
+        self.result = Result()
+        request.result = self.result
         wrk_dir = self.working_directory
         ipa_path = request.download()
 
@@ -81,78 +169,44 @@ class IPArse(ServiceBase):
         if not extract_success:
             return
 
+        with open(os.path.join(os.path.dirname(__file__), "keys.json"), 'r') as f:
+            keys_dict = json.load(f)
+            self.known_keys = keys_dict['glossary']
+
+        if PatternMatch:
+            self.patterns = PatternMatch()
+
         # Info.plist
         main_exe = None
         res = ResultSection(SCORE.NULL, "Info.plist")
         info_plist_path = os.path.join(wrk_dir, isipa)
-        with open(info_plist_path, 'r') as f:
-            info_plist = f.read()
 
-        if info_plist == "":
+        isempty, plist_dict = self.gen_plist_extract(info_plist_path)
+
+        if plist_dict is None:
+            res.add_line("Info.plist in sample cannot be parsed. Sample may not corrupt.")
+
+        elif isempty:
             res.add_line("Empty Info.plist file. Archive contents may be encrypted.")
+
         else:
-
-            with open(os.path.join(os.path.dirname(__file__), "keys.json"), 'r') as f:
-                keys_dict = json.load(f)
-
-            infoplist_common = keys_dict['glossary']
-
-            try:
-                plist_parsed = plistlib.readPlistFromString(info_plist)
-            except:
+            # Grab the main executable name
+            if plist_dict.get("CFBundleExecutable", None):
+                i = plist_dict["CFBundleExecutable"]
                 try:
-                    plist_parsed = biplist.readPlistFromString(info_plist)
-                except Exception as e:
-                    self.log.info("Not a plist:", e)
-                    return
+                    main_exe = (i, "Name of bundle's main executable file: {}".format(i))
+                    res.add_line(main_exe[1])
+                except UnicodeEncodeError:
+                    i = i.encode('utf8', 'replace')
+                    main_exe = (i, "Name of bundle's main executable file: {}".format(i))
+                    res.add_line(main_exe[1])
 
-            known = []
-            unknown = []
-            main_exe = ""
-            for k, i in plist_parsed.iteritems():
-                k_noipad = k.replace("~ipad", "")
-                # Grab the main executable name
-                if k == "CFBundleExecutable":
-                    try:
-                        main_exe = (i, "Name of bundle's main executable file: {}" .format(i))
-                    except UnicodeEncodeError:
-                        i = i.encode('utf8', 'replace')
-                        main_exe = (i, "Name of bundle's main executable file: {}".format(i))
-                elif k_noipad in infoplist_common:
-                    try:
-                        known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
-                    except UnicodeEncodeError:
-                        i = i.encode('utf8', 'replace')
-                        known.append("{} ({}):  {}".format(k, infoplist_common[k_noipad][0], i))
-                else:
-                    try:
-                        unknown.append("{}:  {}".format(k, i))
-                    except UnicodeEncodeError:
-                        i = i.encode('utf8', 'replace')
-                        unknown.append("{}:  {}".format(k, i))
-                    continue
-                if infoplist_common[k_noipad][1]:
-                    if isinstance(i, list):
-                        for val in i:
-                            res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], val, TAG_WEIGHT.LOW)
-                    else:
-                        res.add_tag(TAG_TYPE["PLIST_{}".format(k_noipad.upper())], i, TAG_WEIGHT.LOW)
-
-            if main_exe != "":
-                res.add_line(main_exe[1])
-            if len(known) > 0:
-                idenkey_sec = ResultSection(SCORE.NULL, "Identified Keys", parent=res)
-                for r in sorted(known):
-                    idenkey_sec.add_line(r)
-
-            if len(unknown) > 0:
-                unkkey_sec = ResultSection(SCORE.NULL, "UNIDENTIFIED KEYS:", parent=res)
-                unkkey_sec.add_line("Go to https://developer.apple.com/library/content/documentation/General/Reference"
-                                    "/InfoPlistKeyReference/Introduction/Introduction.html")
-                for r in sorted(unknown):
-                    unkkey_sec.add_line(r)
-
-        result.add_section(res)
+            iden_key_res, unk_key_res = self.parse_plist(plist_dict)
+            if iden_key_res:
+                res.add_section(iden_key_res)
+            if unk_key_res:
+                res.add_section(unk_key_res)
+            self.result.add_section(res)
 
         # PkgInfo file
         pkg_types = {
@@ -180,7 +234,7 @@ class IPArse(ServiceBase):
                         res.add_line("Package Type: {}; Application Signature: {}".format(pkgtype, creator_code))
                     except Exception:
                         continue
-                result.add_section(res)
+                self.result.add_section(res)
 
         if main_exe:
             main_exe_reg = (r'.*{}$' .format(main_exe[0]), "Main executable file {}" .format(main_exe[0]))
@@ -198,6 +252,7 @@ class IPArse(ServiceBase):
 
         empty_file_msg = "Empty file. Archive contents may be encrypted."
         int_files = {}
+        plist_res = ResultSection(SCORE.NULL, "Other Plist File Information")
         for root, dirs, files in os.walk(os.path.join(wrk_dir, "Payload")):
                 for name in files:
                     matched = False
@@ -208,23 +263,39 @@ class IPArse(ServiceBase):
                         else:
                             int_files[empty_file_msg] = []
                             int_files[empty_file_msg].append(full_path)
-                    elif os.path.getsize(full_path) < 50000001:
+                    else:
                         for p, desc in fextract_regs:
                             pattern = re.compile(p)
                             m = pattern.match(full_path.decode("utf8"))
                             if m is not None:
+                                # Already identify main executable file above
                                 if not desc.startswith("Main executable file "):
                                     if int_files.get(desc, None):
                                         int_files[desc].append(full_path)
                                     else:
                                         int_files[desc] = []
                                         int_files[desc].append(full_path)
-                                request.add_extracted(full_path, desc)
+                                    if desc.startswith("Plist"):
+                                        pres = ResultSection(SCORE.NULL, "{}:" .format(full_path))
+                                        isempty, plist_parsed = self.gen_plist_extract(full_path)
+                                        if not isempty and plist_parsed:
+                                            iden_key_res, unk_key_res = self.parse_plist(plist_dict)
+                                            if iden_key_res:
+                                                pres.add_section(iden_key_res)
+                                            if unk_key_res:
+                                                pres.add_section(unk_key_res)
+                                            plist_res.add_section(pres)
+                                if os.path.getsize(full_path) < 50000001:
+                                    request.add_extracted(full_path, desc)
                                 matched = True
                                 break
                         # Extract all files under 5MB
                         if not matched:
-                            request.add_extracted(full_path, "Extracted file")
+                            if os.path.getsize(full_path) < 50000001:
+                                request.add_extracted(full_path, "Extracted file")
+
+        if len(plist_res.subsections) > 0:
+            self.result.add_section(plist_res)
 
         if len(int_files) > 0:
             intf_sec = ResultSection(SCORE.NULL, "Files of interest", parent=res)
